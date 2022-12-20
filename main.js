e25101461e02f334c143e159675643c25268c1c6
tatia <tachile17@gmail.com>
2022-12-20 11:52:22 -0300

    "Maquetando desde javascript"

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon= document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -37,3 +38,61 @@ function toggleCarritoAside(){
     
     aside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price:120,
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: 'Escritorio',
+    price:320,
+    image:"https://compucentro.co/wp-content/uploads/Janus-3.jpg",
+});
+productList.push({
+    name: 'Book',
+    price:20,
+    image:"https://th.bing.com/th/id/R.c9af68cb4356a095987cbb7b7258e5dd?rik=GDQaPyEoHWHrpA&riu=http%3a%2f%2fabrassard.files.wordpress.com%2f2011%2f08%2fbooks.jpg&ehk=ItRzOYOo8vZllTkY1jZxIi9uuVeh6V9MM%2f2ZbEyPHjU%3d&risl=&pid=ImgRaw&r=0",
+});
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', 'curso-frontend-developer-javascript/icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+
+    cardsContainer.appendChild(productCard);
+}
+}
+    
